a7cd7a8816407411d441c438c08fc1eabbda8ec9
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2024-10-31 22:18:33 +0700

    fix: prevent destroy resource, add force option, upgrade-cluster

--- k8s/main.tf
+++ k8s/main.tf
@@ -2,6 +2,23 @@ data "local_file" "inventory_script" {
   filename = "./ansible/scripts/inventory.py"
 }
 
+resource "null_resource" "inventory" {
+  triggers = {
+    kube_control_plane_instances = jsonencode(data.oci_core_instance.kube_control_planes)
+    kube_node_instances          = jsonencode(data.oci_core_instance.kube_nodes)
+  }
+
+  provisioner "local-exec" {
+    command     = <<-EOT
+      python3 scripts/inventory.py \
+        --kube-control-planes '${self.triggers.kube_control_plane_instances}' \
+        --kube-nodes '${self.triggers.kube_node_instances}' \
+        --output-file hosts.yml
+    EOT
+    working_dir = "ansible"
+  }
+}
+
 resource "null_resource" "cluster" {
   triggers = {
     kube_control_plane_instances = jsonencode(data.oci_core_instance.kube_control_planes)
@@ -11,15 +28,22 @@ resource "null_resource" "cluster" {
     ssh_user                     = var.ssh_user
   }
 
+  depends_on = [null_resource.inventory]
+
+  # list tags: https://github.com/kubernetes-sigs/kubespray/blob/master/docs/ansible/ansible.md
   provisioner "local-exec" {
     when        = create
     command     = <<-EOT
-      python3 ../scripts/inventory.py \
-        --kube-control-planes '${self.triggers.kube_control_plane_instances}' \
-        --kube-nodes '${self.triggers.kube_node_instances}' \
-        --output-file ../hosts.yml
-      ansible-playbook -i ../hosts.yml cluster.yml \
+      FILE=cluster.yml
+      if ! [ -f ../.cluster.lock ]; then
+        echo "Creating cluster..."
+      else
+        echo "Cluster already created, upgrading..."
+        FILE=upgrade-cluster.yml
+      fi
+      ansible-playbook -i ../hosts.yml $FILE \
         -v --private-key=${var.ssh_private_key} --become -u ${var.ssh_user}
+      touch ../.cluster.lock
     EOT
     working_dir = "ansible/kubespray"
   }
@@ -27,13 +51,12 @@ resource "null_resource" "cluster" {
   provisioner "local-exec" {
     when        = destroy
     command     = <<-EOT
-      python3 ../scripts/inventory.py \
-        --kube-control-planes '${self.triggers.kube_control_plane_instances}' \
-        --kube-nodes '${self.triggers.kube_node_instances}' \
-        --output-file ../hosts.yml
-      ansible-playbook -i ../hosts.yml reset.yml \
-        -e skip_confirmation=yes -e reset_confirmation=yes \
-        -v --private-key=${self.triggers.ssh_private_key} --become -u ${self.triggers.ssh_user}
+      if [ "$FORCE" == "true" ]; then # only destroy if forced
+        ansible-playbook -i ../hosts.yml reset.yml \
+          -e skip_confirmation=yes -e reset_confirmation=yes \
+          -v --private-key=${self.triggers.ssh_private_key} --become -u ${self.triggers.ssh_user}
+        rm -f ../.cluster.lock
+      fi
     EOT
     working_dir = "ansible/kubespray"
   }
